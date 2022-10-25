4578fee013099de3de860a53101d21091a073f0c
Ali <ali.salloum.gh@gmail.com>
2022-10-25 22:00:46 +0300

    addd LotteryEntered event

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,15 +8,20 @@ contract Lottery {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players; 
 
+    /* Events */
+    event LotteryEntered(address indexed player);
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
     function enterLottery() public payable {
+        // check if the player has enough funds to participate
         if (msg.value < i_entranceFee) {
             revert Lottery__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit LotteryEntered(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
