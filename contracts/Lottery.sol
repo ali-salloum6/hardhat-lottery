99421c28b1cad6c5cc889568029f1443eafb030c
Ali <ali.salloum.gh@gmail.com>
2022-10-26 10:38:36 +0300

    inherited VRFConsumerBaseV2 in Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Lottery__NotEnoughETHEntered();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
-    address payable[] private s_players; 
+    address payable[] private s_players;
 
     /* Events */
     event LotteryEntered(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -24,11 +26,19 @@ contract Lottery {
         emit LotteryEntered(msg.sender);
     }
 
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    /* View/Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer(uint256 index) public view returns(address) {
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
