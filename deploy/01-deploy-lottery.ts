08a2779d63f2310b9a378f280f967dd376da3808
Ali <ali.salloum.gh@gmail.com>
2022-11-04 16:50:42 +0300

    added verify

--- deploy/01-deploy-lottery.ts
+++ deploy/01-deploy-lottery.ts
@@ -2,44 +2,60 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { VERIFICATION_BLOCK_CONFIRMATIONS, networkConfig } from "../helper-hardhat-config";
 import { developmentChains } from "../helper-hardhat-config";
+import verify from "../utils/verify";
+
+const VRF_SUB_FUND_AMOUNT = "1000000000000000000000";
 
 const deployLottery: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts, network, ethers } = hre;
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    let vrfCoordinatorV2Address, subscriptionId;
+    let vrfCoordinatorV2Address: string | undefined, subscriptionId: string | undefined;
 
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
         const tranasctionResponse = await vrfCoordinatorV2Mock.createSubscription();
-        const transactionReceipt = await tranasctionResponse.wait(1);
-        subscriptionId = tranasctionResponse.events[0].subId;
+        const transactionReceipt = await tranasctionResponse.wait();
+        subscriptionId = transactionReceipt.events[0].args.subId;
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT);
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId!].vrfCoordinatorV2;
         subscriptionId = networkConfig[chainId!].subscriptionId;
     }
 
-    const entranceFee = networkConfig[chainId!].lotteryEntranceFee;
+    const lotteryEntranceFee = networkConfig[chainId!].lotteryEntranceFee;
     const gasLane = networkConfig[chainId!].gasLane;
-    subscriptionId = networkConfig[chainId!].subscriptionId;
     const callbackGasLimit = networkConfig[chainId!].callbackGasLimit;
-    const timeInterval = networkConfig[chainId!].keepersUpdateInterval;
+    const keepersUpdateInterval = networkConfig[chainId!].keepersUpdateInterval;
+
     const args: any[] = [
         vrfCoordinatorV2Address,
-        entranceFee,
+        lotteryEntranceFee,
         gasLane,
         subscriptionId,
         callbackGasLimit,
-        timeInterval,
+        keepersUpdateInterval,
     ];
+
+    const confirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS;
+
     const lottery = await deploy("Lottery", {
         from: deployer,
         args: args,
         log: true,
-        waitConfirmations: VERIFICATION_BLOCK_CONFIRMATIONS,
+        waitConfirmations: confirmations,
     });
+
+    if (!developmentChains.includes(network.name)) {
+        log("Verifying...");
+        await verify(lottery.address, args);
+    }
+    log("-----------------------");
 };
 
 export default deployLottery;
+deployLottery.tags = ["all", "lottery"];
